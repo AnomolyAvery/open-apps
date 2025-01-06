6aeffbc267ba9a52d54aade049eb3a7fcd7e3210
AnomolyAvery <ashawsolutions@gmail.com>
2025-01-05 22:48:11 -0500

    Added dynamic auth provider config

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -3,6 +3,8 @@ import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 import { db } from "@/server/db";
+import { Provider } from "next-auth/providers";
+import { env } from "@/env";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -31,18 +33,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+  providers: getEnabledProviders(),
   adapter: PrismaAdapter(db),
   callbacks: {
     session: ({ session, user }) => ({
@@ -54,3 +45,15 @@ export const authConfig = {
     }),
   },
 } satisfies NextAuthConfig;
+
+function getEnabledProviders(): Provider[] {
+  const providers = [];
+
+  if (env.AUTH_DISCORD_ID && env.AUTH_DISCORD_SECRET) {
+    providers.push(DiscordProvider);
+  }
+
+  if (providers.length === 0)
+    throw new Error("No enabled providers were found");
+  return providers;
+}
