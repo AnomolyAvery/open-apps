f05ee8c0a51e33acf650468eab24c459968a9f57
AnomolyAvery <ashawsolutions@gmail.com>
2025-01-05 22:49:41 -0500

    Added shadcn/ui

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="py-8">
+      <div className="container mx-auto max-w-screen-xl px-2.5 md:px-20">
+        <Button>Get Started</Button>
+      </div>
+    </main>
+  );
 }
